b3d17a8c514dd55f0c54342651848526a17dbb13
Jonas Laksen <jonaslaksen@live.com>
2016-10-29 15:23:29 +0200

    add api version in header and allow x requests

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   req.clientId = req.get('Client-Id') || undefined;
+  res.header('Api-Version', process.env.API_VERSION || 0);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "X-Access-Token, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
