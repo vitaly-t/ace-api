c940c3a8fd83839b26bd402f0a514eaac1bcddb4
Jonas Laksen <jonaslaksen@live.com>
2016-11-04 00:48:37 +0100

    Refactor code in subjectDao

--- dao/subjects.js
+++ dao/subjects.js
@@ -5,18 +5,15 @@ const
 
 const findAll = (clientId) => db.any(sql.subjects.findAll, {clientId});
 
-const findById = (subjectId, clientId, showOnlyIfPublished) => {
-    return db.one(sql.subjects.findById, {subjectId, clientId, forceShow: !showOnlyIfPublished})
-        .then(subject => subjectService.processSubject(subject))
-};
+const findById = (subjectId, clientId, showOnlyIfPublished) =>
+    db.one(sql.subjects.findById, {subjectId, clientId, forceShow: !showOnlyIfPublished})
+        .then(subject => subjectService.processSubject(subject));
 
-const addToFavorites = (subjectId, clientId) => {
-    return db.none('insert into favorites (client_id, subject_id) values (${clientId}, ${subjectId})', {clientId, subjectId})
-};
+const addToFavorites = (subjectId, clientId) =>
+    db.none('insert into favorites (client_id, subject_id) values (${clientId}, ${subjectId})', {clientId, subjectId});
 
-const removeFromFavorites = (subjectId, clientId) => {
-    return db.none('delete from favorites where client_id = ${clientId} and subject_id = ${subjectId}', {clientId, subjectId})
-};
+const removeFromFavorites = (subjectId, clientId) =>
+    db.none('delete from favorites where client_id = ${clientId} and subject_id = ${subjectId}', {clientId, subjectId});
 
 module.exports = {
     findAll,
