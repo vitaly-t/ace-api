eba710d83b3aab3055f11d2180345597458e34ac
Jonas Laksen <jonaslaksen@live.com>
2016-11-09 21:20:53 +0100

    Respond with popularity of subject in /subjects

--- sql/subjects/find-all.sql
+++ sql/subjects/find-all.sql
@@ -1,13 +1,12 @@
 -- clientId
 
-select subjects.*, count (exercises.id) as n_exercises, case
+select subjects.*, count (*) as downloads, case
         when ${clientId} in (select client_id from favorites where subject_id = subjects.id)
             then true
             else false
     end as favorite
 from subjects
-join collections on collections.subject_id = subjects.id
-join exercises on collection_id = collections.id
+left join favorites on subjects.id = subject_id
 where published = 'yes'
 group by subjects.id
 order by code;
