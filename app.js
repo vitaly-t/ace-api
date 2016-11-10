acc4d45b35267743610a586556eed82a30f4d5e9
Jonas Laksen <jonaslaksen@live.com>
2016-11-10 23:01:20 +0100

    Allow header platform

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   res.header('Api-Version', process.env.API_VERSION || 0);
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Access-Token, Origin, X-Requested-With, Content-Type, Accept, Client-Id");
+  res.header("Access-Control-Allow-Headers", "X-Access-Token, Origin, X-Requested-With, Content-Type, Accept, Client-Id, Platform");
   next();
 });
 
