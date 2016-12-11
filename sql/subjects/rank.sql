775f581640c5c1d729fd57809697baf3cd02cd9d
Jonas Laksen <jonaslaksen@live.com>
2016-12-11 15:56:01 +0100

    Show newly correctly answered

--- sql/subjects/rank.sql
+++ sql/subjects/rank.sql
@@ -4,7 +4,7 @@ select client_id, count as answered_correct, rank, (
     join exercises on exercise_id=exercises.id
     join collections on collection_id=collections.id
     join subjects on subject_id=subjects.id
-    where answers.created >= now()::date and subjects.id=${subjectId}
+    where answers.modified >= now()::date and subjects.id=${subjectId}
     ) as participants
 from (
     select client_id,count(*), row_number() over (order by count(*) desc) as rank
@@ -15,12 +15,19 @@ from (
     where
     status=true
     and subjects.id=${subjectId}
-    and answers.created >= now()::date
+    and answers.modified >= now()::date
     group by answers.client_id
 ) as a
 where
 client_id=${clientId}
 union all
-select ${clientId} as client_id, '0' as count, '0' as rank, '0' as participants
+select ${clientId} as client_id, '0' as count, '0' as rank,(
+    select count(distinct client_id)
+    from answers
+    join exercises on exercise_id=exercises.id
+    join collections on collection_id=collections.id
+    join subjects on subject_id=subjects.id
+    where answers.modified >= now()::date and subjects.id=${subjectId}
+    ) as participants
 limit 1;
 
