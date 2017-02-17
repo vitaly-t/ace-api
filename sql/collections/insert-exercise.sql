74e1bc67fbbf08e0f34f5b02ce51f5d8a28a655f
Jonas Laksen <jonaslaksen@live.com>
2017-02-17 16:38:02 +0100

    Remove field (crowd_sourced) that is no longer in database

--- sql/collections/insert-exercise.sql
+++ sql/collections/insert-exercise.sql
@@ -3,5 +3,5 @@
 -- type
 
 
-insert into exercises (type, content, collection_id, crowd_sourced, updated_by) values
-(${type}, ${content}, ${collectionId}, true, ${userId}) returning *;
+insert into exercises (type, content, collection_id, updated_by) values
+(${type}, ${content}, ${collectionId}, ${userId}) returning *;
