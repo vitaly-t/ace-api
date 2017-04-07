6a4862431a14a40f07eea2a1cac26992fb56aa16
Jonas Laksen <jonaslaksen@live.com>
2017-04-08 00:47:41 +0200

    Fix quiz bug by adding constraints

--- services/quiz-service.js
+++ services/quiz-service.js
@@ -7,7 +7,7 @@ const deduceRelevance = (skillLevel, initialDifficulty) =>
       w_a = ex.w_a,
       c_a = ex.c_a,
       subjDifficulty = w_m + c_m > 0
-        ? Math.pow(2, w_m - c_m)
+        ? Math.pow(2, w_m - c_m < -10 ? -10 : w_m - c_m > 10 ? 10 : w_m - c_m)
         : initialDifficulty,
       objDifficulty = (1 + w_a) / (1 + w_a + c_a),
       difficulty = objDifficulty * subjDifficulty,
@@ -15,31 +15,54 @@ const deduceRelevance = (skillLevel, initialDifficulty) =>
     return _.extend(ex, {
       difficulty,
       relevance,
-      skillLevel
+      skillLevel,
     });
   };
 
+const tournamentSelection = (
+  xs,
+  evaluate,
+  winners,
+  nRounds,
+  tournamentSize = 3
+) => {
+  if (nRounds === 0) return winners;
+  const winner = _.maxBy(_.sampleSize(xs, tournamentSize), evaluate);
+  return tournamentSelection(
+    _.pull(xs, winner),
+    evaluate,
+    _.union([winner], winners),
+    nRounds - 1,
+    tournamentSize
+  );
+};
+
 const create = (collectionId, exs, quizLength, nAlts) => {
-  const exercises = _.map(_.filter(exs, ex => ex.is_feasible), ex => exerciseService.process(ex, nAlts || 3)),
+  const exercises = _.map(_.filter(exs, ex => ex.is_feasible), ex =>
+    exerciseService.process(ex, nAlts || 3)),
     correctHistory = _.reduce(exercises, (sum, ex) => sum + ex.c_m, 0),
     wrongHistory = _.reduce(exercises, (sum, ex) => sum + ex.w_m, 0),
     skillLevel = 2 *
       (correctHistory / (1 + wrongHistory + correctHistory) - 0.5),
-    measuredExercises = _.map(exercises, deduceRelevance(skillLevel, 4)),
-    lotteryIds = _.shuffle(
-      _.flatten(
-        _.map(measuredExercises, ex =>
-          Array(Math.ceil(100 * ex.relevance)).fill(ex.id))
-      )
-    ),
-    chosenIds = _.uniq(lotteryIds),
-    chosenExercises = _.filter(measuredExercises, ex =>
-      _.includes(chosenIds, ex.id));
+    measuredExercises = _.map(exercises, deduceRelevance(skillLevel, 4));
+  console.log(
+    _.map(measuredExercises, ex => ({ id: ex.id, relevance: ex.relevance }))
+  );
+  const chosenExercises = tournamentSelection(
+    measuredExercises,
+    ex => ex.relevance,
+    [],
+    quizLength,
+    3
+  );
+  console.log(
+    _.map(chosenExercises, ex => ({ id: ex.id, relevance: ex.relevance }))
+  );
 
   return _.take(chosenExercises, quizLength);
   // return _.union(_.take(chosenExercises, quizLength), [{ id: 0, collection_id: collectionId, is_feasible: false, content: { type: 'mc', question: '', alternatives: [] }}]);
 };
 
 module.exports = {
-  create
+  create,
 };
