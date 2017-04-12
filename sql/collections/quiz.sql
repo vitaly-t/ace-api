efb47f2eaa10c09ac61295a2e37e9c7598b676b1
Jonas Laksen <jonaslaksen@live.com>
2017-04-12 18:10:19 +0200

    Much change

--- sql/collections/quiz.sql
+++ sql/collections/quiz.sql
@@ -1,18 +1,17 @@
 select
     exercises.*,
+    exercises.relevance_points > 1 as is_approved,
     users.username as username,
-    (case when approved is true then true else false end) as is_approved,
-    (case when user_credibility is null then false else true end) as has_liked,
+    (case when user_likes_exercise.positive is null then false else true end) as has_liked,
     bool_or(case when a.user_id = ${userId} and a.status = true then true else false end) as status,
     count(case when a.user_id = ${userId} and a.status = true then 1 end) as c_m,
     count(case when a.user_id = ${userId} and a.status = false then 1 end) as w_m,
     count(case when a.status = false then 1 end) as w_a,
     count(case when a.status = true then 1 end) as c_a
 from exercises
-left join exercise_is_approved on exercises.id = exercise_id
 left join answers a on exercises.id = a.exercise_id
 left join users on exercises.updated_by=users.id
 left join user_likes_exercise on exercises.id = user_likes_exercise.exercise_id and user_likes_exercise.user_id = ${userId}
-where collection_id=${collectionId} and (disapproved is null or not disapproved) and (is_official = true or is_official = ${isDaily})
-group by exercises.id, approved, user_likes_exercise.user_credibility, users.username
+where collection_id=${collectionId} and relevance_points > -1
+group by exercises.id, user_likes_exercise.positive, users.username
 order by random()
