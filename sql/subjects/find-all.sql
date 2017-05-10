ae8867f7f9bba8763fe0c51fab450c6d2c01b93e
Jonas Laksen <jonaslaksen@Jonass-MacBook-Pro.local>
2017-05-10 15:03:46 +0200

    Send school name on GET /subjects

--- sql/subjects/find-all.sql
+++ sql/subjects/find-all.sql
@@ -1,12 +1,13 @@
 -- userId
 
-select subjects.*, count (*) as downloads, case
+select subjects.*, schools.name as school, count (*) as downloads, case
         when ${userId} in (select user_id from favorites where subject_id = subjects.id)
             then true
             else false
     end as favorite
 from subjects
 left join favorites on subjects.id = subject_id
+join schools on school_id=schools.id
 where published = 'yes'
-group by subjects.id
+group by subjects.id, schools.name
 order by favorite desc, downloads desc, code;
