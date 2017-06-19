a0bec74ea9a6db98796494885a15a647bc3ab8c7
Jonas Laksen <jonaslaksen@live.com>
2017-06-19 11:37:12 +0200

    Allow cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
