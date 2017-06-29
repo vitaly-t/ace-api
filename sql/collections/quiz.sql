91253d4165d7ce38b5fa2bbf85f7b08c9e88dde0
Jonas Laksen <jonaslaksen@live.com>
2017-06-29 11:09:49 +0200

    fix

--- sql/collections/quiz.sql
+++ sql/collections/quiz.sql
@@ -1,6 +1,6 @@
 select
     exercises.*,
-    (case when user_likes_exercise.positive is null then false else true end) as has_liked,
+    (case when vote_exercise.positive is null then false else true end) as has_liked,
     bool_or(case when a.user_id = ${userId} and a.status = true then true else false end) as status,
     json_build_object('username', users.username) as creator,
     count(case when a.user_id = ${userId} and a.status = true then 1 end) as c_m,
@@ -11,8 +11,8 @@ select
 from exercises
 left join answers a on exercises.id = a.exercise_id
 left join users_view users on exercises.updated_by=users.id
-left join user_likes_exercise on exercises.id = user_likes_exercise.exercise_id and user_likes_exercise.user_id = ${userId}
+left join vote_exercise on exercises.id = vote_exercise.exercise_id and vote_exercise.user_id = ${userId}
 left join comments on comments.exercise_id=exercises.id
 where collection_id=${collectionId}
-group by exercises.id, user_likes_exercise.positive,users.username
+group by exercises.id, vote_exercise.positive,users.username
 order by random()
--- sql/exercises/like.sql
+++ sql/exercises/like.sql
@@ -1,6 +1,6 @@
 -- exerciseId
 -- vote
 
-insert into user_likes_exercise (user_id, exercise_id, positive)
+insert into vote_exercise (user_id, exercise_id, positive)
 values (${userId}, ${exerciseId}, ${positive})
 on conflict do nothing;
