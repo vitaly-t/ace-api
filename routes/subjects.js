d8c283935f3f9887eeebfa302084423d390d3387
Jonas Laksen <jonaslaksen@live.com>
2017-07-04 10:44:31 +0200

    fix

--- routes/subjects.js
+++ routes/subjects.js
@@ -91,7 +91,7 @@ router.get('/:subjectId/ranking', (req, res) => {
     });
 });
 
-router.get('/', [authentication(true), authorization('EDIT_EXERCISE')], (req, res) =>
+router.get('/', [authentication(true)], (req, res) =>
   db
     .any(sql.subjects.findAll, { userId: req.user.id, search: req.query.search || '' })
     .then(subjects => res.status(200).send(normalizr.normalize(subjects, [subjectSchema])))
