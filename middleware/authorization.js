49ad08c08a5d465d661ff29bd7e85d8274a7d80d
Jonas Laksen <jonaslaksen@live.com>
2017-07-06 09:31:15 +0200

    Fix activity

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -5,6 +5,10 @@ module.exports = privilege => (req, res, next) => {
   const p = _.find(req.user.privileges, { name: privilege });
   console.log(p);
   p
-    ? mung.json(body => ({ type: p.name, reinforcement: p.reinforcement }))(req, res, next)
+    ? mung.json(() => ({ activity: { type: p.name, reinforcement: p.reinforcement } }))(
+        req,
+        res,
+        next
+      )
     : res.status(401).send({ message: 'You do not have privileges to perform this activity' });
 };
