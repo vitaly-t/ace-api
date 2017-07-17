f81b2b0188906f09e325f70c4719631386f07812
Jonas Laksen <jonaslaksen@live.com>
2017-07-17 10:41:35 +0200

    preflight

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Access-Token, Origin, X-Requested-With, Content-Type, Accept, Client-Id, Platform'
+  );
   console.log(req.body);
   next();
 });
