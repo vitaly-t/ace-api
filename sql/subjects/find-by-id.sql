8bc7bd0945fa85cdc2fffd937b166d4ce53b809b
Jonas Laksen <jonaslaksen@live.com>
2017-07-19 14:18:03 +0200

    lol

--- sql/subjects/find-by-id.sql
+++ sql/subjects/find-by-id.sql
@@ -2,9 +2,8 @@
 -- subjectId
 -- forceShow: Show subject even though it is not published
 
-select subjects.*, (case when favorites.subject_id is null then false else true end) as favorite, json_agg(collections.*) as topics
+select subjects.*, (case when favorites.subject_id is null then false else true end) as favorite
 from subjects
 left join favorites on favorites.user_id=${userId} and favorites.subject_id=subjects.id
-left join collections on collections.subject_id=subjects.id
 where subjects.id=${subjectId} and published='yes'
 group by subjects.id, favorites.subject_id
