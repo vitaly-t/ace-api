446299d79b44579173d9371f7faca1f888fbbfb9
Jonas Laksen <jonaslaksen@live.com>
2017-07-21 13:05:47 +0200

    ASFDFDS

--- sql/collections/quiz.sql
+++ sql/collections/quiz.sql
@@ -10,6 +10,6 @@ select
 from v_exercises exercises
 left join votes on exercises.id = votes.exercise_id
 where collection_id=${collectionId} -- and not exercises.deleted and not is_disapproved
-group by exercises.id, exercises.content ->> 'question', votes.positive, username, experience, collection_id, subject_id
+group by exercises.id, exercises.content ->> 'question', votes.positive, username, experience, collection_id, subject_id, exercises.user_id
 order by random()
 limit ${size}
