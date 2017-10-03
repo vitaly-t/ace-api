cd77030323403573f59dc82f0983c25d35031a42
Jonas Laksen <jonaslaksen@live.com>
2017-10-03 02:04:43 +0200

    add size to topic

--- sql/collections/find-by-id.sql
+++ sql/collections/find-by-id.sql
@@ -1,5 +1,6 @@
-select collections.*, user_id, username, experience
+select collections.*, user_id, username, experience, coalesce(size,0) as size
 from collections
 join user_owns_resource u on resource_id=collections.id
 join v_users on u.user_id=v_users.id
+left join (select collection_id, count(*) as size from exercises group by collection_id) sz on collection_id=collections.id
 where collections.id=${collectionId}
